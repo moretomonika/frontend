3259e95ad1c1b3fd5dbb549f01c2841432a0220a
moretomonika <monikaaa.15august@gmail.com>
2025-12-06 16:40:05 +0530

    new changes in expense-tracker

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 
+import {
+  BrowserRouter as Router,
+  Routes,Route,Navigate,
+
+} from "react-router-dom"
+
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import AllGroups from "./pages/Dashboard/AllGroups";
+
 const App = () => {
   return (
-    <div>
-      App
+    <div className='text -2xl'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Root/>}></Route>
+          <Route path='/login'  exact element={<Login/>}></Route>
+          <Route path='/signup' element={<SignUp/>}></Route>
+          <Route path='/dashboard' element={<Home/>}></Route>
+          <Route path='/all-groups' element={<AllGroups/>}></Route>
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+const Root=()=>{
+  //check if token exists in local storage
+  const isAuthenticated=!!localStorage.getItem("token");
+
+  //redirect to dashboard if authenticated,otherwise to login
+  return isAuthenticated?(
+    <Navigate to="/dashboard"/>
+  ): (
+    <Navigate to='/login'/>
+  );
+};
---
+++ expense-tracker/src/pages/Dashboard/AllGroups.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllGroups = () => {
+  return (
+    <div>
+      groups
+    </div>
+  )
+}
+
+export default AllGroups
